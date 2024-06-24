a69d9dcfbbf22f3c3da02deb31be369e1af72880
Denys Bystryi <bystryydenis211999@gmail.com>
2024-06-24 17:58:36 +0300

    update-2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,17 +2,21 @@ const formData = { email: '', message: '' };
 const form = document.querySelector('.feedback-form');
 const emailInput = form.elements.email;
 const messageTextarea = form.elements.message;
-const savedData =
-  JSON.parse(localStorage.getItem('feedback-form-state')) || formData;
-emailInput.value = savedData.email;
-messageTextarea.value = savedData.message;
+
+const savedData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+if (savedData.email) emailInput.value = savedData.email;
+if (savedData.message) messageTextarea.value = savedData.message;
+
 form.addEventListener('input', event => {
-  formData[event.target.name] = event.target.value;
+  formData[event.target.name] = event.target.value.trim();
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
+
+  formData.email = emailInput.value.trim();
+  formData.message = messageTextarea.value.trim();
 
   if (!formData.email || !formData.message) {
     alert('Please fill all fields');
@@ -20,6 +24,10 @@ form.addEventListener('submit', event => {
   }
 
   console.log(formData);
+
   localStorage.removeItem('feedback-form-state');
   form.reset();
+
+  formData.email = '';
+  formData.message = '';
 });
